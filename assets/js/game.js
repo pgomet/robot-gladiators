85eb122ae7a50d6b3c28a0a144ca4486ae320188
Pina Gomet <pina@Pinas-MBP.lan>
2022-04-09 01:00:01 -0400

    added fight round

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,7 +9,6 @@ console.log(playerName, playerAttack, playerHealth);
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyAttack = 12;
 
-
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
     // ask player if they'd like to fight or run
@@ -65,8 +64,25 @@ var fight = function(enemyName) {
   } // end of while loop
 }; // end of fight function
 
+
+
 for(var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth=50;
-  fight(pickedEnemyName);
+  if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+    
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+    
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+    
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+  }
+  
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+  }
 }
